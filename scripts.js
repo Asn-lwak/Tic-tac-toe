ea11a1246f0d6425435f09cafbf3c6b5e89a155a
Asn-lwak <asncrafter21032009@gmail.com>
2024-11-11 14:14:42 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -54,29 +54,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-function startGame() {
-    boardArray = ["", "", "", "", "", "", "", "", ""];
-    result.textContent = "";
-    currentPlayer = "X";
+    function startGame() {
+        boardArray = ["", "", "", "", "", "", "", "", ""];
+        result.textContent = "";
+        currentPlayer = "X";
 
-    while (board.firstChild) {
-        board.removeChild(board.firstChild);
-    }
+        while (board.firstChild) {
+            board.removeChild(board.firstChild);
+        }
 
-    for (let i = 0; i < 9; i++) {
-        const cell = document.createElement("div");
-        cell.addEventListener("click", handleClick);
-        cell.style.backgroundColor = "#eee";
-        cell.style.width = "100px";
-        cell.style.height = "100px";
-        cell.style.display = "inline-block";
-        cell.style.border = "1px solid #000";
-        cell.style.fontSize = "2em";
-        cell.style.textAlign = "center";
-        cell.style.lineHeight = "100px";
-        board.appendChild(cell);
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement("div");
+            cell.addEventListener("click", handleClick);
+            cell.style.backgroundColor = "#eee";
+            cell.style.width = "100px";
+            cell.style.height = "100px";
+            cell.style.display = "inline-block";
+            cell.style.border = "1px solid #000";
+            cell.style.fontSize = "2em";
+            cell.style.textAlign = "center";
+            cell.style.lineHeight = "100px";
+            board.appendChild(cell);
+        }
     }
-}
 
     startButton.addEventListener("click", startGame);
 });
