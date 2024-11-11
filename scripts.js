a3556ae9ce0fcef39f567c2567ad28ce48877ab7
Asn-lwak <asncrafter21032009@gmail.com>
2024-11-11 14:40:28 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function startGame() {
         boardArray = ["", "", "", "", "", "", "", "", ""];
         result.textContent = "";
-        currentPlayer = "X";
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
         currentPlayerDisplay.textContent = `Current Player: ${currentPlayer}`;
 
         while (board.firstChild) {
